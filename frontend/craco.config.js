4599c89657721829c244c99d3a27bf8ac89ecdb7
EduardoNogueiradev <eduardocastroemp@gmail.com>
2024-05-06 11:27:05 -0300

    some changes

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,41 +2,39 @@ const { CracoAliasPlugin } = require("react-app-alias");
 const DotEnv = require("dotenv");
 const webpack = require("webpack");
 
-const enviroment = process.env.DEPLOY_ENV || "production";
+const enviroment = process.env.DEPLOY_ENV;
 
 if (enviroment !== "production") {
-  const envPath = DotEnv.config({ path: `/.env.${enviroment}` });
+  const envPath = DotEnv.config({ path: `.env.${enviroment}` });
   if (envPath.error) {
     throw envPath.error;
   }
-} else {
-  const envPath = DotEnv.config({ path: "/.env" });
-  if (envPath.error) {
-    throw envPath.error;
-  }
-}
 
-const env = DotEnv.config({ path: `./.env.${enviroment}` }).parsed;
-const envLocal = DotEnv.config({ path: "./.env.local" }).parsed || {};
+  const env = DotEnv.config({ path: `./.env.${enviroment}` }).parsed;
+  const envLocal = DotEnv.config({ path: "./.env.local" }).parsed || {};
 
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next.trim()}`] = envLocal[next]
-    ? JSON.stringify(envLocal[next].trim())
-    : JSON.stringify(env[next].trim());
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next.trim()}`] = envLocal[next]
+      ? JSON.stringify(envLocal[next].trim())
+      : JSON.stringify(env[next].trim());
 
-  return prev;
-}, {});
+    return prev;
+  }, {});
 
-console.log(`
+  console.log(`
   key: ${enviroment.toLocaleUpperCase()},
   value: ${process.env.REACT_APP_DEPLOY_ENV},
   accumulator: ${envKeys}
 `);
 
+  module.exports = {
+    webpack: {
+      plugins: [new webpack.DefinePlugin(envKeys)],
+    },
+  };
+}
+
 module.exports = {
-  webpack: {
-    plugins: [new webpack.DefinePlugin(envKeys)],
-  },
   plugins: [
     {
       plugin: CracoAliasPlugin,
